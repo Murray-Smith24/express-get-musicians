669e48b72aefe39042940a19b4850f8abd9fceca
Murray-Smith24 <Murray.Smith@southwark.gov.uk>
2024-05-22 10:52:45 +0100

    added code

--- src/app.js
+++ src/app.js
@@ -6,9 +6,34 @@ const { db } = require("../db/connection")
 const port = 3000;
 
 //TODO: Create a GET /musicians route to return all musicians 
-app.get("/musicians", async function(req,res) {
-    const musicReturn = await Musician.findAll();
-    res.send(res.json(musicReturn));
+app.get("/musicians", async (req,res) => {
+    const musicians = await Musician.findAll();
+    res.json(musicians);
+})
+
+app.get("/musicians/:id", async function(req,res) {
+    const number = req.params.id;
+    const musicians = await Musician.findByPk(number);
+    res.json(musicians);
+});
+
+
+//PUT
+app.put("/musicians", async (req,res) => {
+    res.json(await Musician.create(req));
+});
+
+
+//POST
+app.post("/musicians/:id", async (req,res) => {
+    Musician[req.params.id] = req.body.Musician;
+    res.json({Musician});
+})
+
+//DELETE
+app.delete("/musicians/:id", async (req,res) => {
+    await Musician.splice(req.params.id, 1);
+    res.json({Musician});
 })
 
 
